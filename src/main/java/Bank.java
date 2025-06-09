fc210c28820d9f80c394b9d2dd5ffc6cb890ebd7
Joao Labussiere <labussework@gmail.com>
2025-06-09 09:59:45 -0300

    Desafio 3 incompleto

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,2 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
+    private String name;
+    private List<Account> accounts;
+
+    public Bank(String name){
+        this.name = name;
+        this.accounts = new ArrayList<>();
+    }
+
+    public void openAccount(Account account){
+        accounts.add(account);
+    }
+
+    public Account shearchAccount(int number){
+        for (Account account : accounts){
+            if (account.getNumber() == number){
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public void showAccounts(){
+        if (accounts.isEmpty()){
+            System.out.println("Você não tem contas. Crie contas e volte aqui para visualiza-las!");
+        }
+        System.out.println("===LISTAGEM DE CONTAS===");
+        for (Account account: accounts){
+            System.out.println("| Conta: " +account.getNumber() + " - Cliente: " + account.getClient());
+        }
+    }
+
+//    public
+
+
+
+
+
 }
